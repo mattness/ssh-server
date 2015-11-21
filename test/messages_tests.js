aab545d56430fe97fae4cefe1fb802dde480d307
Matt Gollob <mattgollob@gmail.com>
2015-11-21 10:29:11 -0800

    kexinit complete

--- test/messages_tests.js
+++ test/messages_tests.js
@@ -72,7 +72,145 @@ exports.kexInitCreation = {
   },
 
   testArguments: function(t) {
-    t.throws(lib.createKexInit, 'cookie is a required argument');
+    t.throws(lib.createKexInit, /cookie is a required argument/);
+    t.throws(function() {
+      lib.createKexInit("asdf");
+    }, /cookie must be a Buffer/);
+
+    t.throws(function() {
+      lib.createKexInit(new Buffer(15));
+    }, /cookie must be at least 16 bytes/);
+
+    t.throws(function() {
+      lib.createKexInit(new Buffer(16));
+    }, /opts is a required argument/);
+
+    testKexInitAlgorithms(t, 'kexAlgorithms');
+    testKexInitAlgorithms(t, 'serverKeyAlgorithms');
+    testKexInitAlgorithms(t, 'clientEncryptionAlgorithms');
+    testKexInitAlgorithms(t, 'serverEncryptionAlgorithms');
+    testKexInitAlgorithms(t, 'clientMacAlgorithms');
+    testKexInitAlgorithms(t, 'serverMacAlgorithms');
+    testKexInitAlgorithms(t, 'clientCompressionAlgorithms');
+    testKexInitAlgorithms(t, 'serverCompressionAlgorithms');
+
+    t.done();
+  },
+
+  testMessageNum: function(t) {
+    var opts = defaultKexInitOpts();
+    var actual = lib.createKexInit(new Buffer(16), opts);
+    t.equal(actual[0], 20);
+    t.done();
+  },
+
+  testCookie: function(t) {
+    var opts = defaultKexInitOpts();
+    var cookie = new Buffer([
+      0x86, 0xa6, 0x94, 0x2a,
+      0xf8, 0x3a, 0xe8, 0x7a,
+      0x37, 0x45, 0x7e, 0x71,
+      0xcc, 0xbd, 0xba, 0x6f
+    ]);
+
+    var actual = lib.createKexInit(cookie, opts);
+    t.equal(actual.toString('binary', 1, 17), cookie.toString('binary'));
+    t.done();
+  },
+
+  testKexAlgorithms: function(t) {
+    var opts = defaultKexInitOpts();
+    opts.kexAlgorithms = [
+      'diffie-hellman-group1-sha1',
+      'diffie-hellman-group14-sha1'
+    ];
+    var expected = new Buffer([
+      0x00, 0x00, 0x00, 0x36, 0x64, 0x69, 0x66, 0x66,
+      0x69, 0x65, 0x2d, 0x68, 0x65, 0x6c, 0x6c, 0x6d,
+      0x61, 0x6e, 0x2d, 0x67, 0x72, 0x6f, 0x75, 0x70,
+      0x31, 0x2d, 0x73, 0x68, 0x61, 0x31, 0x2c, 0x64,
+      0x69, 0x66, 0x66, 0x69, 0x65, 0x2d, 0x68, 0x65,
+      0x6c, 0x6c, 0x6d, 0x61, 0x6e, 0x2d, 0x67, 0x72,
+      0x6f, 0x75, 0x70, 0x31, 0x34, 0x2d, 0x73, 0x68,
+      0x61, 0x31
+    ]).toString('binary');
+
+    var actual = lib.createKexInit(new Buffer(16), opts);
+    t.equal(actual.slice(17, expected.length + 17).toString('binary'),
+      expected);
+    t.done();
+  },
+
+  testFirstPacketFollows: function(t) {
+    var opts = defaultKexInitOpts();
+    var cookie = new Buffer(16);
+    cookie.fill(1);
+
+    opts.firstPacketFollows = undefined;
+    var actual = lib.createKexInit(cookie, opts);
+    t.equal(actual[57], 0);
+
+    opts.firstPacketFollows = false;
+    actual = lib.createKexInit(cookie, opts);
+    t.equal(actual[57], 0);
+
+    opts.firstPacketFollows = {};
+    actual = lib.createKexInit(cookie, opts);
+    t.equal(actual[57], 1);
+
+    opts.firstPacketFollows = true;
+    actual = lib.createKexInit(cookie, opts);
+    t.equal(actual[57], 1);
+
+    t.done();
+  },
+
+  testReservedByte: function(t) {
+    var opts = defaultKexInitOpts();
+    var cookie = new Buffer(16);
+    cookie.fill(8);
+
+    var actual = lib.createKexInit(cookie, opts);
+    t.equal(actual[58], 0);
     t.done();
   }
 };
+
+function defaultKexInitOpts() {
+  return {
+    kexAlgorithms: [],
+    serverKeyAlgorithms: [],
+    clientEncryptionAlgorithms: [],
+    serverEncryptionAlgorithms: [],
+    clientMacAlgorithms: [],
+    serverMacAlgorithms: [],
+    clientCompressionAlgorithms: [],
+    serverCompressionAlgorithms: [],
+    clientLanguages: [],
+    serverLanguages: []
+  };
+}
+
+function testKexInitAlgorithms(t, nameList) {
+  var opts = defaultKexInitOpts();
+
+  var invalidCharsAlg = '\x00';
+  var tooLongAlg = new Buffer(128);
+  tooLongAlg.fill('a');
+  tooLongAlg = tooLongAlg.toString();
+
+  t.throws(function() {
+    opts[nameList] = [tooLongAlg];
+    lib.createKexInit(new Buffer(16), opts);
+  }, /0 is longer than 64/);
+
+  t.throws(function() {
+    opts[nameList] = ['valid', invalidCharsAlg];
+    lib.createKexInit(new Buffer(16), opts);
+  }, /1 contains invalid/);
+
+  t.throws(function() {
+    opts[nameList] = ['valid', 'alsovalid', ''];
+    lib.createKexInit(new Buffer(16), opts);
+  }, /2 is an empty string/);
+}
